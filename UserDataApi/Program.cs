d9ce7b8fe641340a4af6509726964edfc6b3aa5d
paulius <arxenaut@gmail.com>
2023-04-12 13:41:40 +0300

    Update Program.cs

--- UserDataApi/Program.cs
+++ UserDataApi/Program.cs
@@ -3,15 +3,15 @@ using UserDataApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => {
+    options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+});
 builder.Services.AddDbContext<UserContext>(opt =>
     opt.UseInMemoryDatabase("UserList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -20,11 +20,11 @@ using (var scope = app.Services.CreateScope()) {
     InitData.Initialize(services);
 }
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment()) {
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
