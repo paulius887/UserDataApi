b2e3455ccc4243ed8a3a4651a47a77b629bf7d56
paulius <arxenaut@gmail.com>
2023-04-12 13:40:06 +0300

    Added classes for validation

---
+++ UserDataApi/Validation/EmailIsValid.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using UserDataApi.Models;
+
+namespace UserDataApi.Validation
+{
+    public class EmailIsValid
+    {
+        public static Boolean IsUnique(object value, UserContext _context) {
+            foreach (User user in _context.Users) {
+                if (user.Email.ToString() == value.ToString()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static Boolean IsUnique(object value, int id, UserContext _context) {
+            foreach (User user in _context.Users) {
+                if (user.Email.ToString() == value.ToString() && user.Id != id) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static Boolean HasCorrectFormat(object value) {
+            return Regex.Match(value.ToString(), @"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$").Success;
+        }
+        public static String IsValid(object value, int id, UserContext _context) {
+            if (!IsUnique(value, id, _context)) {
+                return $"Email '{value}' is already in use.";
+            }
+            else if (!HasCorrectFormat(value)) {
+                return $"Email '{value}' is invalid.";
+            }
+            return "";
+        }
+        public static String IsValid(object value, UserContext _context) {
+            if (!IsUnique(value, _context)) {
+                return $"Email '{value}' is already in use.";
+            }
+            else if (!HasCorrectFormat(value)) {
+                return $"Email '{value}' is invalid.";
+            }
+            return "";
+        }
+    }
+}
---
+++ UserDataApi/Validation/UsernameIsValid.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+using UserDataApi.Models;
+
+namespace UserDataApi.Validation
+{
+    public class UsernameIsValid
+    {
+        public static Boolean IsUnique(object value, UserContext _context) {
+            foreach (User user in _context.Users) {
+                if (user.Username.ToString() == value.ToString()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static Boolean IsUnique(object value, int id, UserContext _context) {
+            foreach (User user in _context.Users) {
+                if (user.Username.ToString() == value.ToString() && user.Id != id) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static Boolean HasCorrectFormat(object value) {
+            return Regex.Match(value.ToString(), @"^[a-zA-Z0-9_.]{6,30}$").Success;
+        }
+        public static String IsValid(object value, int id, UserContext _context) {
+            if (!IsUnique(value, id, _context)) {
+               return $"Username '{value}' is already in use.";
+            }
+            else if (!HasCorrectFormat(value)) {
+                return $"Username '{value}' is invalid. Username must be 6-30 characters long, and can only use alphanumeric characters, and special characters _ and .";
+            }
+            return "";
+        }
+        public static String IsValid(object value, UserContext _context) {
+            if (!IsUnique(value, _context)) {
+                return $"Username '{value}' is already in use.";
+            }
+            else if (!HasCorrectFormat(value)) {
+                return $"Username '{value}' is invalid. Username must be 6-30 characters long, and can only use alphanumeric characters, and special characters _ and .";
+            }
+            return "";
+        }
+    }
+}
