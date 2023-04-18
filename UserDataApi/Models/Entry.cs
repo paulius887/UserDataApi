70d2a8f2c82bbc7fc007202078e4aab2cbf62dbe
paulius <arxenaut@gmail.com>
2023-04-18 18:51:55 +0300

    Update Entry.cs

--- UserDataApi/Models/Entry.cs
+++ UserDataApi/Models/Entry.cs
@@ -3,8 +3,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UserDataApi.Models {
     public class Entry : EntryDto {
-        public int Id { get; set; }
         public int UserId { get; set; }
+        public int Id { get; set; }
         public DateTime LastEdited { get; set; }
     }
 }
